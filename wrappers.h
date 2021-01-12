1e7187b4b9091054e40d48613d86dcb4c59042d5
Shengyis <g.shengyis@gmail.com>
2021-01-12 14:28:12 +0800

    add more wrapper functions

--- wrappers.h
+++ wrappers.h
@@ -1,4 +1,4 @@
-// function wrappers. 
+// function wrappers.
 // default to use eigen for vectors/matrix calculation. If other matrix class is used, for example Armadillo, please implement all wrapper functions.
 #pragma once
 #ifdef USE_EIGEN
@@ -14,15 +14,33 @@ namespace mathtool
     }
 
     template <typename T>
-    auto norm(const Eigen::Vector<T, -1> &x)
+    T linearSolve(const T &A, const T &b)
+    {
+        return b / A;
+    }
+
+    template <typename T>
+    auto norm(const Eigen::MatrixBase<T> &x)
     {
         return x.norm();
     }
 
     template <typename T>
-    T linearSolve(const T &A, const T &b)
+    auto transpose(const Eigen::MatrixBase<T> &x)
     {
-        return b / A;
+        return x.transpose();
+    }
+
+    template <typename T>
+    auto operator%(const Eigen::MatrixBase<T> &x, const Eigen::MatrixBase<T> &y)
+    {
+        return x.cwiseProduct(y);
+    }
+
+    template <typename T>
+    auto operator/(const Eigen::MatrixBase<T> &x, const Eigen::MatrixBase<T> &y)
+    {
+        return x.cwiseQuotient(y);
     }
 
     template <typename T>
