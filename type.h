0591f2002edc42a4b024b572c973b0c225b0ca03
shengyis <g.shengyis@gmail.com>
2021-02-27 14:13:47 +0400

    fix a bug

--- type.h
+++ type.h
@@ -138,7 +138,7 @@ namespace mathtool
     template <typename T>
     struct ToVec
     {
-        typedef typename Eigen::Matrix<typename Type<T>::scalar, 1, -1> type;
+        typedef typename Eigen::Matrix<typename Type<T>::scalar, -1, 1> type;
     };
 }; // namespace mathtool
 #endif
