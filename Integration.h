0936d400ecc8b5b4f4b486680d762b46cc175c20
shengyis <g.shengyis@gmail.com>
2021-03-17 08:19:06 +0400

    update

--- Integration.h
+++ Integration.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "wrappers.h"
+#include "types.h"
 
 namespace mathtool
 {
@@ -8,16 +8,16 @@ namespace mathtool
     // input function is f(x(:)) as a vector
     // input x is nodes in [a, b] as a vector, where x(0) = a, x(end) = b.
     // basic data type is double
-    inline void anti_derivative_inplace(const vec_d &in, vec_d &out, const vec_d &x)
+    inline void anti_derivative_inplace(const vec &in, vec &out, const vec &x)
     {
         out(0) = 0.0;
         for (int k = 1; k < x.size(); ++k)
             out(k) = out(k-1) + (in(k) + in(k-1)) / 2.0 * (x(k) - x(k-1)); 
     }
 
-    inline vec_d anti_derivative(const vec_d &in, const vec_d &x)
+    inline vec anti_derivative(const vec &in, const vec &x)
     {
-        vec_d out = vec_d::Zero(x.size());
+        vec out = vec::Zero(x.size());
         anti_derivative_inplace(in, out, x);
         return out;
     }
